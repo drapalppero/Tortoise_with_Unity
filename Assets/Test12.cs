a4358ae476da6abaffc39770bb59d0c01754e2a3
drapalppero <chosd48@naver.com>
2024-11-22 12:02:26 +0900

    dkalkdla

--- Assets/Test12.cs
+++ Assets/Test12.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test11 : MonoBehaviour
+public class Test12 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("ぞ し");
-        Debug.Log("ぞ し"); 
-       
+        Debug.Log("ssssss");
     }
 
     // Update is called once per frame
